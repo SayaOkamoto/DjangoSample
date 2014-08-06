74deff7a1c9160e364f00cc7188cbfef6615f3d1
Saya <Saya_Okamoto@voyagegroup.com>
2014-08-06 15:47:26 +0900

    tutorial04の最後（リファクタリング）まで

--- polls/views.py
+++ polls/views.py
@@ -9,20 +9,6 @@ from polls.models import Choice, Poll
 
 # Create your views here.
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html',
-            {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p},
-            context_instance=RequestContext(request))
-
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/results.html', {'poll': p})
-
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
@@ -39,4 +25,4 @@ def vote(request, poll_id):
         # ユーザが Back ボタンを押して同じフォームを提出するのを防ぐ
         # ため、POST データを処理できた場合には、必ず
         # HttpResponseRedirect を返すようにします。
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
