2dc4983525c5de5a8771528886cd13336b140efd
Saya <Saya_Okamoto@voyagegruop.com>
2014-08-06 14:53:14 +0900

    tutorial03の終わりまで

--- django_sample/urls.py
+++ django_sample/urls.py
@@ -4,14 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_sample.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
-
-from django.conf import settings
